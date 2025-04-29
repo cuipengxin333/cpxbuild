9b38643029e4ddb9309930b103bd517e7ed5f20d
cuiepngxin <cuipengxin@autohome.com.cn>
2025-04-29 15:54:47 +0800

    feat: 玩玩看

---
+++ cpxself/src/view/ShowCase/components/UnloadAnimation/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text">努力编写中...</div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 20px;
+  color: var(--text-primary);
+}
+</style>
